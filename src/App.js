dc995ed03c80b3004eef5c91b04f09fca0fe4f43
Gil <85408560+Gipeio@users.noreply.github.com>
2024-05-15 23:39:51 +0200

    responsive grid ui

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import * as Sparql from './Sparql';
 
 function App() {
   return (
-    <div className="bg-blue-200 min-h-screen relative">
-      <div class="absolute inset-0 flex items-center justify-center">
-        <div class="size-4/6">
-          <ContentPanel/>
-        </div>
+    <div className="bg-blue-200 relative grid grid-cols-10 grid-rows-10 h-screen">
+      <div className='bg-green-200 col-span-1 row-span-10'></div>
+      <div className='bg-gray-200 col-start-2 row-span-1 col-span-8'></div>
+      <div class="row-start-2 col-start-2 row-span-8 col-span-8  overflow-hidden">   
+        <ContentPanel/>  
       </div>
+      <div className='row-start-10 col-start-2 bg-gray-200 col-span-8 row-span-1'></div>
+      <div className='bg-red-200 col-start-10 col-span-1 row-span-10'></div>
     </div>
   );
 }
