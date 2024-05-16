fea88f917e2bbcaac9695b3784085bcd0f3e0d90
W-Trinh <71597125+W-Trinh@users.noreply.github.com>
2024-05-16 09:29:56 +0200

    add query to get event from every european countries

--- src/Sparql.js
+++ src/Sparql.js
@@ -13,9 +13,36 @@ async function queryDbpedia(query) {
   }
 
 export async function getCountries(){
-    const data = await queryWikidata('PREFIX wdt:<http://www.wikidata.org/prop/direct/> PREFIX wd:<http://www.wikidata.org/entity/> SELECT ?idcountry ?label WHERE {?idcountry wdt:P31 wd:Q6256. ?idcountry wdt:P361 wd:Q458. ?idcountry rdfs:label ?label. FILTER((LANG(?label)) = "en")}');
-    for (const elt of data){
-        console.log(elt.idcountry.value)
-        console.log(elt.label.value)
+    const wallahi = await queryWikidata(
+        'PREFIX wdt:<http://www.wikidata.org/prop/direct/>'+
+        'PREFIX wd:<http://www.wikidata.org/entity/>'+
+        'SELECT ?idcountry ?label WHERE'+
+        '{?idcountry wdt:P31 wd:Q6256.'+
+        '?idcountry wdt:P361 wd:Q458.'+
+        '?idcountry rdfs:label ?label.'+
+        'FILTER((LANG(?label)) = \"en\")}'
+    );
+
+    for (const elt of wallahi){
+        getHistoricalEventFromCountry(elt.idcountry.value, elt.label.value)
     }
+}
+
+export async function getHistoricalEventFromCountry(country, countryName){
+    const wallahi = await queryWikidata(
+        'PREFIX wdt:<http://www.wikidata.org/prop/direct/>'+
+        'PREFIX wd:<http://www.wikidata.org/entity/>'+
+        'SELECT ?event ?label ?start ?end WHERE {'+
+          '?event wdt:P31/wdt:P279* wd:Q13418847.'+
+          '?event rdfs:label ?label;'+
+            'wdt:P17 <' + country + '>.'+
+          //'?event wdt:P31 ?theme.'+
+          //'?theme rdfs:label ?themeLabel.'+
+          '?event wdt:P580 ?start.'+
+          '?event wdt:P582 ?end.'+
+          //'FILTER((LANG(?themeLabel)) = \"en\")'+
+          'FILTER((LANG(?label)) = \"en\")}'
+    )
+        console.log(countryName)
+        console.log(wallahi)
 }
