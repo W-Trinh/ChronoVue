fe58d7203454bd0b25c7d525eb1c5d91a7c2d984
W-Trinh <trinh.williams084@gmail.com>
2024-05-18 16:16:04 +0200

    separating queries

--- src/services/Sparql.js
+++ src/services/Sparql.js
@@ -1,17 +1,28 @@
 require('sparqljs')
 
+/*
+    Function used to query Wikidata API
+    Should not be used outside of this file
+*/
 async function queryWikidata(query){
     const response = await fetch('https://query.wikidata.org/sparql?format=json&query=' + query);
     const data = await response.json();
     return data.results.bindings;
 }
 
+/*
+    Function used to query Dbpedia API
+    Should not be used outside of this file
+*/
 async function queryDbpedia(query) {
     const response = await fetch('http://dbpedia.org/sparql?format=json&query=' + query);
     const data = await response.json();
     return data.results.bindings;
   }
 
+/*
+    Get every country of the european union
+*/
 export async function getCountries(){
     let result = {}
     const wallahi = await queryWikidata(
@@ -25,48 +36,97 @@ export async function getCountries(){
     );
 
     for (const country of wallahi){
-        result[country.label.value] = await getHistoricalEventFromCountry(country.idcountry.value, country.label.value)
+        result[country.label.value] = {id : country.idcountry.value}
     }
-
+    console.log(result)
     return(result)
 }
 
-export async function getHistoricalEventFromCountry(country, countryName){
+/*
+    Get every event from a country
+    Should be used in the home and content page 
+    @param country : The ID of the country inside the Wikidata Database
+*/
+export async function getHistoricalEventFromCountry(country){
     let result = {}
     const wallahi = await queryWikidata(
         'PREFIX wdt:<http://www.wikidata.org/prop/direct/>'+
         'PREFIX wd:<http://www.wikidata.org/entity/>'+
-        'SELECT ?event ?label ?themeLabel ?start ?end ?freebase ?image WHERE {'+
+        'SELECT DISTINCT ?event ?start ?end ?label ?desc WHERE {'+
           '?event wdt:P31/wdt:P279* wd:Q13418847.'+
           '?event rdfs:label ?label;'+
             'wdt:P17 <' + country + '>;'+
-            'wdt:P646 ?freebase.'+
-          '?event wdt:P31 ?theme.'+
-          '?theme rdfs:label ?themeLabel.'+
-          '?event wdt:P580 ?start.'+
-          '?event wdt:P582 ?end.'+
-          '?event wdt:P18 ?image.'+
-          'FILTER((LANG(?themeLabel)) = \"en\")'+
-          'FILTER((LANG(?label)) = \"en\")}'
+            'schema:description ?desc;'+
+            'wdt:P580 ?start;'+
+            'wdt:P582 ?end.'+
+          'FILTER((LANG(?label)) = \"en\")}' +
+          'LIMIT 1'
     )
 
     for (const event of wallahi){
-        if (typeof(result[event.label.value]) === 'undefined'){
-            const abstract = await getAbstractOfEvent(event.freebase.value)
-            result[event.label.value] = {
-                'start':event.start.value,
-                'end':event.end.value,
-                'theme': [event.themeLabel.value],
-                'abstract': abstract,
-                'image': event.image.value,
-            }
-        } else {
-            result[event.label.value]['theme'].push(event.themeLabel.value)
+        result[event.label.value] = {
+            id: event.event.value,
+            start: event.start.value,
+            end: event.end.value,
+            desc: event.desc.value,
         }
     }
     return result
 }
 
+/*
+    Get the information of an event 
+    Should be used in the content page to display informations
+    @param event : The ID of the event inside the Wikidata Database
+*/
+export async function getInfoOfEvent(event){
+    let result = {}
+    const wallahi = await queryWikidata(
+        'PREFIX wdt:<http://www.wikidata.org/prop/direct/>'+
+        'PREFIX wd:<http://www.wikidata.org/entity/>'+
+        'SELECT DISTINCT ?label ?start ?end ?freebase ?image ?themeLabel ?desc WHERE {'+
+            '<' + event + '> rdfs:label ?label;'+
+            'schema:description ?desc;'+
+            'wdt:P31 ?theme;'+
+            'wdt:P18 ?image;'+
+            'wdt:P580 ?start;'+
+            'wdt:P582 ?end.'+
+          'OPTIONAL {<' + event + '>  wdt:P646 ?freebase}'+
+          '?theme rdfs:label ?themeLabel.'+
+          'FILTER((LANG(?themeLabel)) = \"en\")'+
+          'FILTER((LANG(?label)) = \"en\")}'
+    )
+    console.log(wallahi)
+    for (const event of wallahi){
+        
+        if (typeof(result[event.label.value]) === 'undefined'){
+
+            let abstract = "undefined"
+            if (typeof(event.freebase) !== 'undefined'){
+                abstract = await getAbstractOfEvent(event.freebase.value)
+            } else {
+                abstract = event.desc.value
+            }
+
+            result[event.label.value] = {
+                'start':event.start.value,
+                'end':event.end.value,
+                'theme': [event.themeLabel.value],
+                'abstract': abstract,
+            }
+        } else {
+            result[event.label.value]['theme'].push(event.themeLabel.value)
+        }
+    }
+
+    return(result)
+}
+
+/*
+    Get the abstract of an event from Dbpedia
+    Used to link Dbpedia and Wikidata
+    @param freebaseId : The ID of the event inside the Freebase ID.
+*/
 export async function getAbstractOfEvent(freebaseId){
     const wallahi = await queryDbpedia(
         'PREFIX dbo: <http://dbpedia.org/ontology/>'+
