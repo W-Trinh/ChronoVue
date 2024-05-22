657789d1e2a19dd05df051c96fdccd0d26b18ac0
W-Trinh <71597125+W-Trinh@users.noreply.github.com>
2024-05-22 10:51:22 +0200

    Optimize time of query (#8)
    * getCountries
    
    * separating queries
    
    * test
    
    * reduce loading time
    
    * better request
    
    * add request based on date
    
    * display fetched info for event
    
    * better link between dbpedia and wikidata
    
    * add language const
    
    * remove useless query
    
    * cleanup
    
    ---------
    
    Co-authored-by: gipeio <gipeio22@gmail.com>

--- src/pages/ContentPage.js
+++ src/pages/ContentPage.js
@@ -1,57 +1,70 @@
 import ContentPanel from '../components/ContentPanel';
 import { useLocation  } from 'react-router-dom';
 import EventCard from '../components/EventCard';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { getInfoOfEvent } from '../services/Sparql';
 
 
   function ContentPage() {
-
     const location = useLocation();
     const { eventkey, event } = location.state;
+    const [data, setData] = useState({});
 
-    return (
-      <div className="relative bgImage grid grid-cols-10 grid-rows-10 h-screen">
-        <div className='flex col-span-2 row-span-10 pr-8 pl-8'>
-          <div className='flex justify-center flex-col space-y-10 w-full h-full w-2/8'>
-            <div className='h-1/4'>
-              <EventCard eventkey={eventkey} event={event}/>
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await getInfoOfEvent(event["id"])
+        setData(result)
+      };
+      fetchData();
+    }, []);
+
+    if(Object.keys(data).length === 0){
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="relative bgImage grid grid-cols-10 grid-rows-10 h-screen">
+          <div className='flex col-span-2 row-span-10 pr-8 pl-8'>
+            <div className='flex justify-center flex-col space-y-10 w-full h-full w-2/8'>
+              <div className='h-1/4'>
+                <EventCard eventkey={eventkey} event={event}/>
+              </div>
+              <div className='h-1/4'>
+                <EventCard eventkey={eventkey} event={event}/>
+              </div>
+              <div className='h-1/4'>
+                <EventCard eventkey={eventkey} event={event}/>
+              </div>
             </div>
-            <div className='h-1/4'>
-              <EventCard eventkey={eventkey} event={event}/>
-            </div>
-            <div className='h-1/4'>
-              <EventCard eventkey={eventkey} event={event}/>
+          </div>
+
+          <div className="flex col-span-6 w-full justify-center items-center pt-8">
+              <div className="w-1/2">
+                <a href="/">
+                  <button className="button-27" role="button">
+                      Retour au tirage
+                  </button>
+                </a>
+              </div>
+          </div>
+          <div class="row-start-2 col-start-3 row-span-9 col-span-6 overflow-hidden p-8">
+            <ContentPanel title={eventkey} event={data}/>
+          </div>
+          <div className='flex col-start-9 col-span-2 row-span-10 pr-8 pl-8'>
+            <div className='flex justify-center flex-col space-y-10 w-full h-full w-2/8'>
+              <div className='h-1/4'>
+                <EventCard eventkey={eventkey} event={event}/>
+              </div>
+              <div className='h-1/4'>
+                <EventCard eventkey={eventkey} event={event}/>
+              </div>
+              <div className='h-1/4'>
+                <EventCard eventkey={eventkey} event={event}/>
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="flex col-span-6 w-full justify-center items-center pt-8">
-            <div className="w-1/2">
-              <a href="/">
-                <button className="button-27" role="button">
-                    Retour au tirage
-                </button>
-              </a>
-            </div>
-        </div>
-        <div class="row-start-2 col-start-3 row-span-9 col-span-6 overflow-hidden p-8">
-          <ContentPanel title={eventkey} content={event["abstract"]} image={event["image"]}/>
-        </div>
-        <div className='flex col-start-9 col-span-2 row-span-10 pr-8 pl-8'>
-          <div className='flex justify-center flex-col space-y-10 w-full h-full w-2/8'>
-            <div className='h-1/4'>
-              <EventCard eventkey={eventkey} event={event}/>
-            </div>
-            <div className='h-1/4'>
-              <EventCard eventkey={eventkey} event={event}/>
-            </div>
-            <div className='h-1/4'>
-              <EventCard eventkey={eventkey} event={event}/>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 
 export default ContentPage;
--- src/services/Sparql.js
+++ src/services/Sparql.js
@@ -1,17 +1,28 @@
-require('sparqljs')
+const language = "en"
 
+/*
+    Function used to query Wikidata API
+    Should not be used outside of this file
+*/
 async function queryWikidata(query){
     const response = await fetch('https://query.wikidata.org/sparql?format=json&query=' + query);
     const data = await response.json();
     return data.results.bindings;
 }
 
+/*
+    Function used to query Dbpedia API
+    Should not be used outside of this file
+*/
 async function queryDbpedia(query) {
     const response = await fetch('http://dbpedia.org/sparql?format=json&query=' + query);
     const data = await response.json();
     return data.results.bindings;
   }
 
+/*
+    Get every country of the european union
+*/
 export async function getCountries(){
     let result = {}
     const wallahi = await queryWikidata(
@@ -21,61 +32,123 @@ export async function getCountries(){
         '{?idcountry wdt:P31 wd:Q6256.'+
         '?idcountry wdt:P361 wd:Q458.'+
         '?idcountry rdfs:label ?label.'+
-        'FILTER((LANG(?label)) = \"en\")}'
+        'FILTER((LANG(?label)) = \"'+language+'\")}'
     );
 
     for (const country of wallahi){
-        result[country.label.value] = await getHistoricalEventFromCountry(country.idcountry.value, country.label.value)
+        result[country.label.value] = await getHistoricalEventFromCountry(country.idcountry.value)
     }
-
     return(result)
 }
 
-export async function getHistoricalEventFromCountry(country, countryName){
+/*
+    Get every event from a country
+    Should be used in the home and content page 
+    @param country : The ID of the country inside the Wikidata Database
+*/
+export async function getHistoricalEventFromCountry(country, date, when){
+    let filterDate = ""
+    if (when==="before"){
+        filterDate = "FILTER (?start > \""+ date +"\"^^xsd:dateTime)"
+    } else if (when==="after"){
+        filterDate = "FILTER (?end < \""+ date +"\"^^xsd:dateTime)"
+    }
+
     let result = {}
     const wallahi = await queryWikidata(
         'PREFIX wdt:<http://www.wikidata.org/prop/direct/>'+
         'PREFIX wd:<http://www.wikidata.org/entity/>'+
-        'SELECT ?event ?label ?themeLabel ?start ?end ?freebase ?image WHERE {'+
+        'SELECT DISTINCT ?event ?start ?end ?label ?desc ?image WHERE {'+
           '?event wdt:P31/wdt:P279* wd:Q13418847.'+
           '?event rdfs:label ?label;'+
             'wdt:P17 <' + country + '>;'+
-            'wdt:P646 ?freebase.'+
-          '?event wdt:P31 ?theme.'+
-          '?theme rdfs:label ?themeLabel.'+
-          '?event wdt:P580 ?start.'+
-          '?event wdt:P582 ?end.'+
-          '?event wdt:P18 ?image.'+
-          'FILTER((LANG(?themeLabel)) = \"en\")'+
-          'FILTER((LANG(?label)) = \"en\")}'
+            'schema:description ?desc;'+
+            'wdt:P580 ?start;'+
+            'wdt:P582 ?end;'+
+            'wdt:P18 ?image.'+
+            filterDate +
+          'FILTER((LANG(?desc)) = \"'+language+'\")'+  
+          'FILTER((LANG(?label)) = \"'+language+'\")}'
     )
 
     for (const event of wallahi){
+        result[event.label.value] = {
+            id: event.event.value,
+            start: event.start.value,
+            end: event.end.value,
+            abstract: event.desc.value,
+            image: event.image.value,
+        }
+    }
+    console.log(result)
+    return result
+}
+
+/*
+    Get the information of an event 
+    Should be used in the content page to display informations
+    @param event : The ID of the event inside the Wikidata Database
+*/
+export async function getInfoOfEvent(eventArg){
+    let result = {}
+    const wallahi = await queryWikidata(
+        'PREFIX wdt:<http://www.wikidata.org/prop/direct/>'+
+        'PREFIX wd:<http://www.wikidata.org/entity/>'+
+        'SELECT DISTINCT ?label ?start ?end ?freebase ?image ?themeLabel ?desc WHERE {'+
+            '<' + eventArg + '> rdfs:label ?label;'+
+            'schema:description ?desc;'+
+            'wdt:P31 ?theme;'+
+            'wdt:P18 ?image;'+
+            'wdt:P580 ?start;'+
+            'wdt:P582 ?end.'+
+          'OPTIONAL {<' + eventArg + '>  wdt:P646 ?freebase}'+
+          '?theme rdfs:label ?themeLabel.'+
+          'FILTER((LANG(?desc)) = \"'+language+'\")'+
+          'FILTER((LANG(?themeLabel)) = \"'+language+'\")'+
+          'FILTER((LANG(?label)) = \"'+language+'\")}'
+    )
+    for (const event of wallahi){
+        
         if (typeof(result[event.label.value]) === 'undefined'){
-            const abstract = await getAbstractOfEvent(event.freebase.value)
+
+            let abstract = "undefined"
+            if (typeof(event.freebase) !== 'undefined'){
+                abstract = await getAbstractOfEvent(eventArg)
+            } else {
+                abstract = event.desc.value
+            }
+
             result[event.label.value] = {
-                'start':event.start.value,
-                'end':event.end.value,
-                'theme': [event.themeLabel.value],
-                'abstract': abstract,
-                'image': event.image.value,
+                start:event.start.value,
+                end:event.end.value,
+                theme: [event.themeLabel.value],
+                abstract: abstract,
+                image: event.image.value,
             }
         } else {
             result[event.label.value]['theme'].push(event.themeLabel.value)
         }
     }
-    return result
+
+    return(result)
 }
 
-export async function getAbstractOfEvent(freebaseId){
+/*
+    Get the abstract of an event from Dbpedia
+    Used to link Dbpedia and Wikidata
+    @param Event : The event inside the Wikidata
+*/
+export async function getAbstractOfEvent(event){
+    console.log(event)
     const wallahi = await queryDbpedia(
         'PREFIX dbo: <http://dbpedia.org/ontology/>'+
         'SELECT ?abstract WHERE {'+
-        '?o owl:sameAs <http://rdf.freebase.com/ns/m.'+freebaseId.substring(3)+'>;'+
+        '?o owl:sameAs <'+event+'>;'+
             'dbo:abstract ?abstract.'+
-        'FILTER((LANG(?abstract )) = \"en\")'+
+        'FILTER((LANG(?abstract )) = \"'+language+'\")'+
         '}'
     )
+    console.log(wallahi)
     if(wallahi.length>0){
         return(wallahi[0].abstract.value)
     } else {
